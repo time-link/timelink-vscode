73b229d8e18c19db5fe522cfc5e414f2bc5eca18
João Carvalho <joaomcarvalho@sapo.pt>
2019-10-29 15:37:03 +0000

    Load hover information from json. Includes documentation server updates.

--- src/extension.ts
+++ src/extension.ts
@@ -7,9 +7,9 @@ import { FormattingProvider } from "./formatting";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const hoverProvider: HoverProvider.HoverContent = new HoverProvider.HoverContent();
+	const hoverProvider: HoverProvider.HoverContent = new HoverProvider.HoverContent(context);
 	const formattingProvider: FormattingProvider.Formatting = new FormattingProvider.Formatting();
-
+	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "kleio" is now active!');
--- src/hover.ts
+++ src/hover.ts
@@ -1,43 +1,102 @@
 import * as vscode from 'vscode';
 
+import docJsonFile from './StructureDocumentation.json';
+
 export module HoverProvider {
+  const DocJsonKey = "DocJsonKey";
+  const DocJsonUrl = "https://docs.google.com/uc?export=download&id=1QW1-G34sxTwv4-mROCudJJRgt2N6JQ0B";
+  var docJson: object;
 
   export class HoverContent {
-    hoverDetails: Map<string, string>;
 
-    constructor() {
-      this.hoverDetails = new Map<string, string>();
+    constructor(context: vscode.ExtensionContext) {
+      if (context.workspaceState.get(DocJsonKey) !== undefined) {
+        console.log("Cached documentation exists.");
+        docJson = context.workspaceState.get(DocJsonKey) as object;
+        // console.log(typeof docJson);
+        // console.log(docJson);
+      } else {
+        // use predefined documentation values
+        docJson = docJsonFile;
+      }
 
-      this.hoverDetails.set("kleio", "kleio\
-        also=structure,translator,autorels,obs,prefix,translations;\
-        position=structure,translator,obs;");
+      // check for notation updates from Kleio server
+      this.checkNotationUpdates(context);
+    }
 
-      this.hoverDetails.set("fonte", "fonte\
-        guaranteed=id;\
-        also=tipo,loc,localizacao,ref,data,ano,obs;");
+    writeToFile() {
+      var fs = require('fs');
+      fs.readFile('readMe.txt', 'utf8', function (err: any, data: any) {
+        fs.writeFile('writeMe.txt', data, function (err: any, result: any) {
+          if (err) {
+            console.log('error', err);
+          }
+        });
+      });
+    }
 
-      this.hoverDetails.set("bap", "Baptismos\
-        position=id,dia,mes,ano,fol,local,celebrante;\
-        guaranteed=id,dia,mes,ano;\
-        repeat=celebrante,n,\
-        test,referido,referida");
+    checkNotationUpdates(context: vscode.ExtensionContext) {
+      console.log('Connecting to Kleio Server');
+      var rp = require('request-promise');
 
-      this.hoverDetails.set("cas", "Casamentos\
-        name=cas,termo;\
-        source=pt-acto;\
-        guaranteed=id,dia,mes,ano,fol;\
-        position=id,dia,mes,ano,fol,loc,celebrante;\
-        also=celebrante,obs;\
-        repeat=celebrante,test,referida,referido,");
+      var options = {
+        method: 'GET',
+        uri: DocJsonUrl,
+        json: true
+      };
+
+      rp(options)
+        .then(function (parsedBody: any) {
+          context.workspaceState.update(DocJsonKey, parsedBody);
+          docJson = context.workspaceState.get(DocJsonKey) as object;
+        })
+        .catch(function (err: any) {
+          console.log("Couldn't connect to Kleio Server?");
+        });
+    }
+
+    // get key value from json data
+    getValue(key: string, data: object) {
+      if (data.hasOwnProperty(key)) {
+        var idx = Object.keys(data).indexOf(key);
+        if (idx >= 0) {
+          return Object.values(data)[idx];
+        }
+      }
     }
 
     getHoverContent(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
-      const hoveredWord = document.getText(document.getWordRangeAtPosition(position));
-      console.log(hoveredWord)
-      const tokenText = this.hoverDetails.get(hoveredWord)
-      //return (tokenText) ? tokenText : null;
+      var hoveredWord = document.getText(document.getWordRangeAtPosition(position));
+      var tokenText = null;
+      var values = [];
+
+      var tmp = this.getValue("groups", docJson) as object;
+      if (tmp !== undefined && tmp.hasOwnProperty(hoveredWord)) {
+
+        var idx = Object.keys(tmp).indexOf(hoveredWord);
+        if (idx >= 0) {
+          console.log(Object.values(tmp)[idx].minimal);
+          console.log(Object.values(tmp)[idx].complete);
+          console.log(Object.values(tmp)[idx].includes);
+
+          var hovertext = "**" + hoveredWord + "**";
+          values.push(hovertext);
+          
+          hovertext = "";
+          hovertext += "\r\n\r\n**Minimal:** " + Object.values(tmp)[idx].minimal;
+          hovertext += "\r\n\r\n**Complete:** " + Object.values(tmp)[idx].complete;
+          hovertext += "\r\n\r\n**Typical:** " + Object.values(tmp)[idx].typical;
+          hovertext += "\r\n\r\n**Includes:** " + Object.values(tmp)[idx].includes;
+          values.push(hovertext);
+
+          tokenText = "[link to Documentation!](http://google.com) ";
+
+          values.push(tokenText);
+        }
+      }
+
       return {
-        contents: (tokenText) ? ['*' + hoveredWord + "*", tokenText] : []
+        contents: (tokenText) ? values : []
       };
     }
   }
